ab9d35bbfe83a3f1eb655172335a808ed159b3d9
IAleks94 <andrei@vetric.io>
2026-04-14 15:11:10 +0200

    0.2.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export const VERSION = "0.2.0";
+export const VERSION = "0.2.1";
 
 export * from "./decorators/index.js";
 export { toZodSchema, validate, validateSafe } from "./schema-builder.js";
